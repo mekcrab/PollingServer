61419d7cbf53b42fefd48685ade56894da3f7572
Erik <erik@minimonk.(none)>
2013-07-15 00:37:20 -0400

    setting up config

--- Pollster.py
+++ Pollster.py
@@ -6,10 +6,11 @@ Created on Jul 3, 2013
 
 from flask import Flask
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
